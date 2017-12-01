01b839590fe1a74159e4f76169e4aad0613893c6
13514103 muhammadreifiza <13514103@std.stei.itb.ac.id>
2017-12-01 19:39:50 +0700

    update gitignore + add distance matrix

--- agglomerative.py
+++ agglomerative.py
@@ -1,3 +1,7 @@
+import math
+import pandas as pd
+from itertools import islice
+
 def linkage_tree(X, n_clusters=None, linkage='single', return_distance=False):
     return True
 
@@ -22,8 +26,7 @@ def _average_group_linkage(*args, **kwargs):
     return linkage_tree(*args, **kwargs)
 
 
-_TREE_BUILDERS = dict(
-    single=_single_linkage,
+_TREE_BUILDERS = dict(single=_single_linkage,
     complete=_complete_linkage,
     average=_average_linkage,
     average_group=_average_group_linkage)
@@ -43,4 +46,44 @@ class Agglomerative:
             raise ValueError("Unknown linkage type %s."
                              "Valid options are %s" % (self.linkage,
                                                        _TREE_BUILDERS.keys()))
+        # Initialize data to X
+        self.data = X
+        # Calculate distance matrix
+        self.distance_matrix = []
+        self.init_distance_matrix()
+
+
+
         tree_builder = _TREE_BUILDERS[self.linkage]
+
+    def init_distance_matrix(self):
+        """ Buat distance matrix antar instans data pada dataset.
+
+        Bentuk distance matrix : [[dist_01, ... , dist_0n], [dist_12, ... , dist_1n],...,[dist_{n-k}{n-k+1},...dist_{n_k}{n}],...[dist_{n-1}{n}]
+        dimana 0 < k <= n """
+        for i, col in enumerate(self.data):
+            eval_instance = self.data.iloc[i]
+            distance_array = []
+            for j, col2 in islice(enumerate(self.data), i, None):
+                pair_instance = self.data.iloc[j]
+                distance = self.calculate_distance(eval_instance, pair_instance)
+                distance_array.append(distance)
+            self.distance_matrix.append(distance_array)
+
+    def calculate_distance(self, instance1, instance2):
+        """ Hitung jarak antara instance1 dengan instance2
+
+        Instance1 & instance2 diasumsikan ada di satu dataset yang sama.
+        Jarak dihitung dengan metode euclidean : sqrt(sum((atr_instance1 - atr_instance2)^2)) """
+        distance = 0
+        for attribute, val in instance1.items():
+            attr_distance = (val -  instance2[attribute])**2
+            distance += attr_distance
+        return math.sqrt(distance) 
+        
+if __name__ ==  "__main__" : 
+    #import pandas as pd
+    test_data = {'A' : [1, 3, 2], 'B' : [2, 4, -1]}
+    test_dataframe = pd.DataFrame(test_data)
+    test_agglomerative = Agglomerative()
+    test_agglomerative.fit(test_dataframe)
